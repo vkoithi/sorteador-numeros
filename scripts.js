6c7c05bb6624d136c9e87da27c3649df8a066b84
Victor Koithi <koithivictor@gmail.com>
2026-09-17 14:47:33 -0300

    Finalizando estilização, partindo para o Javascript

---
+++ scripts.js
@@ -0,0 +1,33 @@
+const quantity = document.querySelector("#quantity")
+const minimum = document.querySelector("#minimum")
+const maximum = document.querySelector("#maximum")
+const noRepeat = document.querySelector("#no-repeat")
+
+const form = document.querySelector("form")
+const draw = document.getElementById("draw")
+
+form.onsubmit = (event) => {
+    event.preventDefault()
+
+    const quantityValue = Number(quantity.value)
+    const minimumInput = Number(minimum.value)
+    const maximumInput = Number(maximum.value)
+
+    if (minimumInput > maximumInput) {
+        alert("Não foi possível fazer o sorteio")
+        return
+    } 
+
+    const availableNumbers = maximumInput - minimumInput + 1
+
+    if (quantityValue > availableNumbers && noRepeat.checked) {
+        alert("Não há número suficientes para sortear sem repetição")
+        return
+    }
+
+    Math.floor(Math.random()* 4) + 1
+
+    const randomNumber
+
+    draw.classList.add("drawing")
+}
